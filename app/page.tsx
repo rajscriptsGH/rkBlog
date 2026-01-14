203ada45fb901b0b11adf79ade8ebe3a055ff8dd
rajyadav63 <vector36razz@gmail.com>
2026-01-14 13:27:04 +0545

    navbar completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,7 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <div className="min-h-screen w-full bg-[#020617] relative">
-      <div className="absolute inset-0 "
-        style={{
-          background: "#020617",
-          backgroundImage: `
-        linear-gradient(to right, rgba(71,85,105,0.15) 1px, transparent 1px),
-        linear-gradient(to bottom, rgba(71,85,105,0.15) 1px, transparent 1px),
-        radial-gradient(circle at 50% 60%, rgba(236,72,153,0.15) 0%, rgba(168,85,247,0.05) 40%, transparent 70%)
-      `,
-          backgroundSize: "40px 40px, 40px 40px, 100% 100%",
-        }}
-      />
-      <h1>Welcome to rkblog</h1>
-    </div>
+    <div></div>
   );
 }
---
+++ components/web/navbar.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { buttonVariants } from "../ui/button";
+
+export function Navbar() {
+    return (
+        <nav className="w-full py-5 flex items-center justify-between">
+            <div className="flex items-center">
+                <Link href="/">
+                    <h1 className="text-3xl font-bold">rk<span className="text-orange-500">Blog</span></h1>
+                </Link>
+
+                <div className="flex items-center gap-2">
+                    <Link className={buttonVariants({ variant: "ghost" })} href="/">Home</Link>
+                    <Link className={buttonVariants({ variant: "ghost" })} href="/blog">Blog</Link>
+                    <Link className={buttonVariants({ variant: "ghost" })} href="/create">Create</Link>
+                </div>
+            </div>
+
+            <div className="flex items-center gap-2">
+                <Link className={buttonVariants({ variant: "outline" })} href="/auth/login">Login</Link>
+                <Link className={buttonVariants()} href="/auth/sign-up">Signup</Link>
+            </div>
+        </nav >
+    )
+}
